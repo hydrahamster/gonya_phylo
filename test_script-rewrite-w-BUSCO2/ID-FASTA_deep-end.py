67723432be8750caabb5a1ecc12c2d1ef61546d4
ALK <anna.kretzschmar@uts.edu.au>
2017-04-20 14:24:50 +1000

    gggr argh

--- test_script-rewrite-w-BUSCO2/ID-FASTA_deep-end.py
+++ test_script-rewrite-w-BUSCO2/ID-FASTA_deep-end.py
@@ -6,18 +6,36 @@ from glob import glob
 uniqueids = open('BUSCOs-unique-single.tsv', 'r')
 colnames = ['a']
 df = pandas.read_csv(uniqueids, sep='\t', names=colnames)
-goodids = df.a.tolist()
+goodids = df.a.tolist() #list of common BUSCO IDs
 masterfile = open('BUSCOs-complete-frag.tsv', 'r')
 mornames = ['a', 'b', 'c', 'd', 'e', 'f']
 df = pandas.read_csv(masterfile, sep='\t', names=mornames)
 buscoid = df.a.tolist()
 contigid = df.c.tolist()
-sourcetrans = df.f.tolist()
-buscodict = zip(contigid , buscoid)
-for file in glob('run_*/translated_proteins/*.faa'):
-	proteingoop = file
-	if goodids in buscodict:
+#sourcetrans = df.f.tolist() #source of contig
+buscodict = zip(contigid , buscoid) #dictionary with contig as key and buscoID as value kk
+for thing in goodids: #somewhere reapeat loop has crept in
+	with open(thing +'.fasta', 'w') as fasta:
+		for file in glob('run_*/translated_proteins/*.faa'): #looking through all sub directories 
+			proteingoop = file.split("/")[2] #^all trnaslated contig files kk
+			for entry in buscodict:
+#				if buscodict.get(entry) == thing:
+#					ergh = buscodict.keys() #get key
+#			if thing in buscodict:
+#				get key and match to proteingoop
+#				append proteingoop to fasta
+	#if goodid in buscodict value, find associated key
+	#use key to get associated proteingoop file
+	#append file contents to goodid.fasta
 
+					print buscodict
+#for file in glob('run_*/translated_proteins/*.faa'): #looking through all sub directories 
+#	proteingoop = file #^all trnaslated contig files
+#	if goodids in buscodict: #if common BUSCO in value then
+#maybe need for loop here to make it work
+#		thing = buscodict.get(goodids) #get the associated key to 
+
+#		print buscodict
 #make dictionary with contigs as key, buscoids as values (use zip of 2 lists)
 #scan good ids through dict values, if hit use key to extract relevant proteingoop (.faa)
 #append .faa file to new file w/ good id name
@@ -28,14 +46,14 @@ for file in glob('run_*/translated_proteins/*.faa'):
 #	buscoid = l[0] #BUSCO
 #	contigid = l[3] #transcript
 #	sourcetrans = l[5] #transcriptome source
-for file in glob('run_*/translated_proteins/*.faa'):
-	contigfile = file
-	for thing in goodids:
-		with open(thing + '.fasta', 'w') as fasta:
-			with open(thing + '_master.csv', 'w') as master:
-				if thing in buscoid:
-					master.write(line.strip())
-					master.write("\n")
+#for file in glob('run_*/translated_proteins/*.faa'):
+#	contigfile = file
+#	for thing in goodids:
+#		with open(thing + '.fasta', 'w') as fasta:
+#			with open(thing + '_master.csv', 'w') as master:
+#				if thing in buscoid:
+#					master.write(line.strip())
+#					master.write("\n")
 						
 
 
